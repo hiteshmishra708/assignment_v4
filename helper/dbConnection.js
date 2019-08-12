6933248cc355f690da8967e4e3c2383accb7ea22
Hitesh Mishra <hiteshmishra708@gmail.com>
2019-08-12 13:17:07 +0530

    Node changes

--- helper/dbConnection.js
+++ helper/dbConnection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const constants = require('./constant')
+const FileModel = require('../models/index');
 async function connectDB() {
 	try {
 		let check = await mongoose.connect(constants.dburl, { useNewUrlParser: true });
@@ -9,4 +10,17 @@ async function connectDB() {
 	}
 }
 
+async function saveFileData(fileObj) {
+	let fileModel = new FileModel(fileObj);
+	await fileModel.save()
+		.then(async res => {
+			console.log(res);
+		})
+		.catch(err => {
+			console.log(err);
+			winston.error('Error saving the data in mongodb ' + err);
+		})
+}
+
 module.exports.connectDB = connectDB
+module.exports.saveFileData = saveFileData
--- server.js
+++ server.js
@@ -53,18 +53,6 @@ app.get('/', function (req, res, next) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-async function saveFileData(fileObj) {
-	let fileModel = new FileModel(fileObj);
-	await fileModel.save()
-		.then(async res => {
-			console.log(res);
-		})
-		.catch(err => {
-			console.log(err);
-			winston.error('Error occured while inserting data ' + err);
-		})
-}
-
 app.post('/files', upload.any(), async function (req, res, next) {
 	if (!req.files) {
 		return next(new Error('No files uploaded'))
@@ -81,20 +69,19 @@ app.post('/files', upload.any(), async function (req, res, next) {
 		try {
 			let data = fs.readFileSync(fileObj.file_path);
 			let records = JSON.parse(data);
-			let tsKey = null, valKey = null, element = null;
+			let tsKey = null, valKey = null;
 			if(records.length > 0) {
-				element = records[0]
-				Object.keys(element).forEach(key => {
-					if (!tsKey) tsKey = validation.isValidTimeStamp(element, key)? key: null
-					if (!valKey) valKey = validation.isValidValue(element, key)? key: null
+				Object.keys(records[0]).forEach(key => {
+					if (!tsKey) tsKey = validation.isValidTimeStamp(records[0], key)? key: null
+					if (!valKey) valKey = validation.isValidValue(records[0], key)? key: null
 				});
 			}
 			let i = records.length; 
 			while(i--) fileObj.data[new Date((records[i])[tsKey]).toISOString().slice(0, 10)] = (records[i])[valKey]
+			fileObj['data'] = fileObj.data
 			fileObj['tsKey'] = tsKey? tsKey: 'failed to get timestamp col'
 			fileObj['valKey'] = valKey? tsKey: 'failed to get value col'
-			fileObj['data'] = fileObj.data
-			saveFileData(fileObj)
+			dbConnection.saveFileData(fileObj)
 		} catch (err) {
 			console.log(err)
 			winston.error("Error when extracting the data " + err)
@@ -106,7 +93,6 @@ app.post('/files', upload.any(), async function (req, res, next) {
 		'status': true,
 		'data': resData
 	}
-	console.log(response)
 	res.status(200).json(response)
 })
 
@@ -128,7 +114,6 @@ app.get('/getfiles', function (req, res, next) {
 app.post('/getfileinfo', jsonParser, function (req, res, next) {
 	FileModel.findOne({ _id: req.body._id })
 		.then(async resData => {
-			console.log(resData)
 			let response = {
 				'status': true,
 				'data': [resData]
